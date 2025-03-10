1191ec2c11f79c9b7f093f1d049eab5aa5112051
agonzago <agonzago@gmail.com>
2025-03-09 21:59:27 -0400

    Cleaning up the folder

--- Simplest/+qpm_simpl1/dynamic_set_auxiliary_series.m
+++
@@ -1,7 +0,0 @@
-function ds = dynamic_set_auxiliary_series(ds, params)
-%
-% Computes auxiliary variables of the dynamic model
-%
-ds.AUX_ENDO_LEAD_80=ds.DLA_CPI(1);
-ds.AUX_ENDO_LEAD_44=ds.AUX_ENDO_LEAD_80(1);
-end
